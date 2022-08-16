2fdf37199c9be2ed96a402e4800f3a5706b6ab74
Alejandro Hernandez Toro <1006009372@u.icesi.edu.co>
2022-08-16 17:35:03 -0500

    Added files linked from school computer

---
+++ prueba-vue-intro/src/App.vue
@@ -0,0 +1,57 @@
+<script>
+  import ToDoList from "./components/ToDoList.vue"
+
+  export default 
+  {
+    components: { ToDoList }
+  }
+</script>
+
+<template>
+  <header class="header">
+    <h2>To Do List</h2>
+  </header>
+
+  <main>
+    <ToDoList />
+  </main>
+</template>
+
+<style scoped lang="scss">
+
+$colorS: #034146;
+$textColorHeader: #FFFFFF;
+
+header {
+  line-height: 1.5;
+  background: $colorS;
+  color: $textColorHeader;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ prueba-vue-intro/src/components/ToDoList.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          name: "Ir a la Icesi",
+          isDone: false,
+        },
+        {
+          name: "Hacer tareas",
+          isDone: false,
+        },
+        {
+          name: "Ir a clase",
+          isDone: true,
+        },
+      ]
+    };
+  },
+  methods: {
+    onListItemClick(task) {
+      console.log("HJello");
+      task.isDone = true;
+      console.log(task.isDone);
+    },
+
+    onListOver(task){
+        console.log("Howdy");
+    }
+  },
+  computed: {
+    doneTasks() {
+      return this.tasks.filter((item) => item.isDone);
+    },
+    undoneTasks() {
+      return this.tasks.filter((item) => !item.isDone);
+    },
+  },
+};
+</script>
+
+<template>
+  <h2>Todas las tareas</h2>
+  <ul>
+    <li
+      class="task"
+      :class="{}"
+      v-for="task in tasks"
+      :key="task.name"
+      @click="() => onListItemClick(task)"
+      @mouseenter="() => onListOver(task)"
+    >
+      {{ task.name }}
+      <span v-if="task.isDone">😜</span>
+      <span v-else-if="task.name.includes('Icesi')">👀</span>
+      <span v-else>🤢</span>
+    </li>
+  </ul>
+
+  <h3>Tareas Terminadas</h3>
+    <ul>
+    <li
+      class="task"
+      :class="{}"
+      v-for="task in doneTasks"
+      :key="task.name"
+      @click="() => onListItemClick(task)"
+    >
+      {{ task.name }}
+    </li>
+  </ul>
+
+  <h3>Tareas sin Terminar</h3>
+    <ul>
+    <li
+      class="task"
+      :class="{}"
+      v-for="task in undoneTasks"
+      :key="task.name"
+      @click="() => onListItemClick(task)"
+    >
+      {{ task.name }}
+    </li>
+  </ul>
+</template>
+
+<style>
+</style>
